fa6ca8a0940eeee233abb0c7ce1523950738da30
Iwona <iwona@gmail.com>
2020-04-04 15:16:10 +0200

    Zakomentowanie get method

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ app = FastAPI()
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
-@app.get("/method")
-def read_method():
-    return {"method": "METHOD"}
+#@app.get("/method")
+#def read_method():
+#    return {"method": "METHOD"}
 
 
 
